092170ba6b199c390c3d0c050679ee2caa4c585d
Marcos Hernández <marcosdavidho@outlook.es>
2024-09-18 04:18:35 +0400

    Quick fix

--- script.js
+++ script.js
@@ -29,25 +29,3 @@ function fade(){
     element.offsetHeight; // Animation restart
     element.style.animation = "fadeIn 0.25s";
 }
-
-
-function smoothScroll(target) {
-    const element = document.getElementById(target);
-    if (element) {
-      window.scrollTo({
-        top: element.offsetTop,
-        behavior: 'smooth'
-      });
-    }
-  }
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const anchors = document.querySelectorAll('a[href^="#"]');
-    anchors.forEach(function(anchor) {
-      anchor.addEventListener('click', function(event) {
-        event.preventDefault();
-        const target = anchor.getAttribute('href').substring(1);
-        smoothScroll(target);
-      });
-    });
-  });
